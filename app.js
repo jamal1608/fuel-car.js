a7dc7d28c925be07ddfce10fe094f012583c0f4d
jamal1608 <ahmedbhatti909@gmail.com>
2024-03-02 11:39:15 +0500

    fuel car js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let remainingFuel = prompt("Enter the remaining fuel in your car (in litres):");
+
+if (remainingFuel < 0.25) {
+    alert("Please refill the fuel in your car");
+} else {
+    alert("You have sufficient fuel. Have a safe journey!");
+}
+
+
+ 
